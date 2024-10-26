a9e63e1084aedf392d500ad995844c0b281383ef
mifuda-tech <mifuda.tech@gmail.com>
2024-10-27 00:44:16 +0900

    import css

--- src/components/Cloudy.jsx
+++ src/components/Cloudy.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Cloudy = () => {
   return (
     <div>
       <h1>cloudy</h1>
+      <div className="cloud"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Cloudy
+export default Cloudy;
