cb2ff6dad6727ae25355362abbc8a9674ba2dba0
mifuda-tech <mifuda.tech@gmail.com>
2024-10-27 01:43:01 +0900

    import in Cloudy.jsx

--- src/components/Cloudy.jsx
+++ src/components/Cloudy.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import background from "../images/background.png";
 import cloud from "../images/cloud.png";
+import head from "../images/head.png";
+import rightarm from "../images/right-arm.png";
+import leftarm from "../images/left-arm.png";
+import rightleg from "../images/right-leg.png";
+import leftleg from "../images/left-leg.png";
 
 const Cloudy = () => {
   return (
@@ -8,6 +13,13 @@ const Cloudy = () => {
       <h1>cloudy</h1>
       <img src={cloud} className="cloud" />
       <img src={background} className="home" />
+      <div className="character">
+        <img src={head} className="head" />
+        <img src={rightarm} className="right-arm" />
+        <img src={leftarm} className="left-arm" />
+        <img src={rightleg} className="right-leg" />
+        <img src={leftleg} className="left-leg" />
+      </div>
     </div>
   );
 };
