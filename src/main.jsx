3edef45946fb4eeca22e9428d005bedf4f0260f1
uma1123 <yuma1596@gmail.com>
2024-10-27 11:21:20 +0900

    o

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Main_Screen from "./Main_Screen";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Main_Screen />
+      <App />
     </Router>
   </StrictMode>
 );
